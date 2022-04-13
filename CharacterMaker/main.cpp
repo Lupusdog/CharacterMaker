d29bd20d13fdcef0f66c779f57e4c611e3ebea0f
Lupusdog <gc.jp1130@gmail.com>
2022-04-13 12:04:31 +0900

    Initial Commit

---
+++ CharacterMaker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CharacterMaker
+//
+//  Created by 清水日向 on 2022/04/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
